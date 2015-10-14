c814c3479d7c4d38d7e20177242335eeb76a0fa8
Benoit Arnaud <ben.ol.69@hotmail.fr>
2015-10-14 12:18:50 +0200

    Update DL.py

--- DL.py
+++ DL.py
@@ -1,1 +1,20 @@
-#Disponible le mardi 13 octobre.
+import matplotlib.pyplot as plp
+import numpy as np
+
+plp.grid()
+
+x=np.linspace(0,3,100)
+
+for n in range(20):
+    plp.plot(x, x**(n+1)-2*x**n+1)
+    plp.axis([0,3,-1,1])
+    plp.plot([0,3],[0,0])
+    plp.plot([2,2],[-1,1],label='borne Sup')
+    plp.plot ([(2*n)/(n+1),(2*n)/(n+1)],[-1,1],label='borne Inf')
+    plp.legend()
+    plp.text(2.6,-0.8,'n={}'.format(n))
+    plp.grid()
+    plp.show()
+    plp.pause(0.5)
+    plp.clf()
+
