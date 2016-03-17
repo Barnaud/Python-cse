6c5610b122ee4d46c5d9139d8bf2e38e6cfde38b
Benoit Arnaud <ben.ol.69@hotmail.fr>
2016-03-17 23:14:15 +0100

    Create SysEq.py

---
+++ SysEq.py
@@ -0,0 +1,13 @@
+l= [[1,2,8],[0,3,1],[0,0,2]]
+m= [3,2,1]
+
+def resoudre(l,m):
+    s=[0,0,0]
+    for k in range(len(l)):
+        reste = 0
+        for n in range(len(l[0])):
+            reste+=s[n]*l[-k-1][n]
+        print(reste)
+        s[-k-1]= (1/l[-k-1][-k-1])*(m[-k-1]-reste)
+        
+    return(s)
