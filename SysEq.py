985f527ab12c76991feba61726ba610c436d9d00
Benoit Arnaud <ben.ol.69@hotmail.fr>
2016-03-18 07:34:22 +0100

    Update SysEq.py

--- SysEq.py
+++ SysEq.py
@@ -2,7 +2,7 @@ l= [[1,2,8],[0,3,1],[0,0,2]]
 m= [3,2,1]
 
 def resoudre(l,m):
-    s=[0,0,0]
+    s=[0 for k in range(len(l))]
     for k in range(len(l)):
         reste = 0
         for n in range(len(l[0])):
