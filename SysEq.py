987f1d077f5fbeb57646096486a6ffbd25b61841
Benoit Arnaud <ben.ol.69@hotmail.fr>
2016-03-29 15:02:00 +0200

    Update SysEq.py

--- SysEq.py
+++ SysEq.py
@@ -1,7 +1,7 @@
 l= [[2,2,8],[1,3,1],[3,0,2]]
 m= [3,2,1]
 
-def resoudre(l,m):
+def resoudre(l):
     s=[0 for k in range(len(l))]
     for k in range(len(l)):
         reste = 0
@@ -22,8 +22,11 @@ def produitListe(liste, nb):
 def difference(l1,l2):
     return([l1[k]-l2[k] for k in range(len(l1))])
 
-def triangle(liste,m):
+def triangle(liste):
     for i in range(1,len(liste)):
         for k in range(i,len(liste)):
             liste[k]=difference(liste[k],produitListe(liste[i-1],liste[k][i-1]/liste[i-1][i-1]))
-            print(liste)
+    return(liste)
+            
+def fin(liste):
+    return(resoudre(triangle(liste)))
